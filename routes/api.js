af3af5df9483ed4768b62ad14ad4c8953c473965
Francisco <fvsalapic@outlook.com>
2025-02-10 16:32:59 +0100

    formulario añadir nuevo punto de interés

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,18 @@ router.get("/points", function (req, res, next) {
   res.json(pointsOfInterest);
 });
 
+// Ruta para añadir un nuevo punto de interés
+router.post("/points", function (req, res, next) {
+  const newPoint = {
+    id: pointsOfInterest.length + 1,
+    name: req.body.name,
+    description: req.body.description,
+    latitude: parseFloat(req.body.latitude),
+    longitude: parseFloat(req.body.longitude),
+  };
+
+  pointsOfInterest.push(newPoint);
+  res.json(newPoint);
+});
+
 module.exports = router;
